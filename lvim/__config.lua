ea8c36e496f8b9a2da431b6bbb913cda571d1ac7
Sergey Yakubovskiy <serg.yakubovskiy@gmail.com>
2022-02-25 20:23:11 +0300

    update dotconfigs

--- lvim/__config.lua
+++ lvim/__config.lua
@@ -1,53 +1,21 @@
--- general
-lvim.log.level = "warn"
+--[[
+lvim is the global options object
+
+Linters should be
+filled in as strings with either
+a global executable or a path to
+an executable
+]]
+
+-- *
+-- GENERAL
+-- *
+lvim.log.level = "info"
 lvim.format_on_save = true
-lvim.colorscheme = "onedarker"
--- lvim.colorscheme = "palenight"
-
--- keymappings [view all the defaults by pressing <leader>Lk]
-lvim.leader = "space"
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-lvim.keys.normal_mode["Q"] = "<NOP>"
-lvim.keys.normal_mode["<S-Tab>"] = ":bnext<CR>"
-lvim.keys.normal_mode["<Tab>"] = ":bprevious<CR>"
-lvim.keys.normal_mode["<S-F>"] = ":RnvimrToggle<CR>"
--- add your own keymapping
-lvim.builtin.dashboard.active = true
-lvim.builtin.notify.active = true
-lvim.builtin.terminal.active = true
-lvim.builtin.terminal.float_opts = {
-      border = "curved",
-      width = 170,
-      height = 41,
-      winblend = 0,
-      highlights = {
-        border = "Normal",
-        background = "Normal",
-      },
-}
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
-
--- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
-}
-
-lvim.builtin.treesitter.ignore_install = { "haskell" }
-lvim.builtin.treesitter.highlight.enabled = true
-
-
+-- lvim.colorscheme = "tokyonight"
+lvim.colorscheme = "palenight"
+-- lvim.colorscheme = "onedarker"
+--- ccheck
 vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
 vim.opt.cmdheight = 1 -- more space in the neovim command line for displaying messages
 vim.opt.colorcolumn = "99999" -- fixes indentline for now
@@ -92,6 +60,21 @@ vim.opt.spelllang = "en"
 vim.opt.scrolloff = 8 -- is one of my fav
 vim.opt.sidescrolloff = 8
 
+-- *
+-- KEYMAPPINGS [view all the defaults by pressing <leader>Lk]
+-- *
+lvim.leader = "space"
+-- add your own keymapping
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["Q"] = "<NOP>"
+lvim.keys.normal_mode["<S-Tab>"] = ":bnext<CR>"
+lvim.keys.normal_mode["<Tab>"] = ":bprevious<CR>"
+lvim.keys.normal_mode["<S-F>"] = ":RnvimrToggle<CR>"
+          -- vim.api.nvim_set_keymap('n', '-', ':RnvimrToggle<CR>', {noremap = true, silent = true})
+-- unmap a default keymapping
+-- lvim.keys.normal_mode["<C-Up>"] = ""
+-- edit a default keymapping
+-- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
@@ -110,7 +93,6 @@ lvim.builtin.telescope.defaults.mappings = {
     ["<C-k>"] = actions.move_selection_previous,
   },
 }
-
 
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["z"] = { "<cmd>ZenMode<CR>", "Zen Mode" }
@@ -131,19 +113,95 @@ lvim.builtin.which_key.mappings["v"] = {
   c = { "<cmd>:set list!<cr>", "toggle list" },
 }
 
+-- TODO: User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.dashboard.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.terminal.float_opts = {
+      border = "curved",
+      width = 170,
+      height = 41,
+      winblend = 0,
+      highlights = {
+        border = "Normal",
+        background = "Normal",
+      },
+}
+lvim.builtin.nvimtree.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 1
 lvim.builtin.bufferline.active = true
 lvim.builtin.lualine.active = true
 lvim.builtin.dap.active = false
 lvim.builtin.telescope.active = true
 lvim.builtin.autopairs.active = true
+-- lvim.builtin.
+-- lvim.builtin.compe.allow_prefix_unmatch = false
+-- lvim.builtin.compe.autocomplete = true
+-- lvim.builtin.compe.debug = false
 lvim.builtin.telescope.defaults.layout_config.width = 0.95
 lvim.builtin.telescope.defaults.layout_config.preview_cutoff = 75
 
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
+
+
 -- generic LSP settings
 lvim.lsp.automatic_servers_installation = true
+-- you can set a custom on_attach function that will be used for all the language servers
+-- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+-- you can overwrite the null_ls setup table (useful for setting the root_dir function)
+-- lvim.lsp.null_ls.setup = {
+--   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
+-- }
+-- or if you need something more advanced
+-- lvim.lsp.null_ls.setup.root_dir = function(fname)
+--   if vim.bo.filetype == "javascript" then
+--     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
+--       or require("lspconfig/util").path.dirname(fname)
+--   elseif vim.bo.filetype == "php" then
+--     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
+--   else
+--     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
+--   end
+-- end
 
+-- set a formatter if you want to override the default lsp one (if it exists)
+-- lvim.lang.python.formatters = {
+--   {
+--     exe = "black",
+--   }
+-- }
+-- set an additional linter
+-- lvim.lang.python.linters = {
+--   {
+--     exe = "flake8",
+--   }
+-- }
 
-
+-- *
+-- LANG
+-- *
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   { exe = "goimports" },
@@ -327,12 +385,18 @@ lvim.plugins = {
       event = "BufRead",
     },
 }
-
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 lvim.autocommands.custom_groups = {
   { "BufWinEnter", "*", "set listchars=tab:→\\ ,space:·,nbsp:␣,trail:•,eol:¶,precedes:«,extends:»" },
 }
--- lvim.autocommands.custom_groups = {
---   { "BufWinLeave", "*", "mkview" },
---   { "BufWinEnter", "*", "silent! loadview" },
--- }
+lvim.autocommands.custom_groups = {
+  { "BufWinLeave", "*", "mkview" },
+  { "BufWinEnter", "*", "silent! loadview" },
+}
+-- zf - Apply fold to the selected lines
+-- zo - Open the folded lines
+-- zc - Close the open lines back to fold
+-- zM - Close all folds
+-- zR - Open all folds
+-- zE - Delete all the folds
+-- zd - Delete the fold under the cursor
